1ad400490172d20e6f1290078948c0d3096ce210
QuanThaiX <145018752+QuanThaiX@users.noreply.github.com>
2024-12-08 20:05:09 +0700

    Update loader.py

--- src/loader.py
+++ src/loader.py
@@ -95,7 +95,7 @@ class MyDataLoader:
 
             new_tokens = []
             contexts_A = []
-            print(input_token)
+            print(input_tokens)
             for i, line in enumerate(input_tokens):
                 print(line)
                 line = ' '.join(line.split()[:self.config.max_length - 25])
